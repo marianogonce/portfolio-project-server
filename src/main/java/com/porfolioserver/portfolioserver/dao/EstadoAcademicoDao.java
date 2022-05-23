9d455384211dc3df3704aa82f8aaeab13c00c2f3
marianogonce <73374381+marianogonce@users.noreply.github.com>
2022-05-22 23:49:59 -0300

    Delete src/main/java/com/porfolioserver/portfolioserver directory

--- src/main/java/com/porfolioserver/portfolioserver/dao/EstadoAcademicoDao.java
+++
@@ -1,9 +0,0 @@
-package com.porfolioserver.portfolioserver.dao;
-
-import com.porfolioserver.portfolioserver.models.EstadoAcademico;
-
-import java.util.List;
-
-public interface EstadoAcademicoDao {
-    List<EstadoAcademico> getEstadoAcademicos();
-}
--- src/main/java/com/porfolioserver/portfolioserver/dao/HardSkillLevelDao.java
+++
@@ -1,9 +0,0 @@
-package com.porfolioserver.portfolioserver.dao;
-
-import com.porfolioserver.portfolioserver.models.HardSkillLevel;
-
-import java.util.List;
-
-public interface HardSkillLevelDao {
-    List<HardSkillLevel> getHardSkillsLevels();
-}
